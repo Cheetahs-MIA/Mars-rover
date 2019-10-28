06165e96345685c47fbd0c00d4237cc0e5836a5f
--replace-all <ignaciobellomo@gmail.com>
2019-10-28 14:37:03 -0400

    created class

--- rovers.js
+++ rovers.js
@@ -1,1 +1,9 @@
-
+class Rover {
+    constructor(name, direction, x, y) {
+        this.name = name;
+        this.direction = direction;
+        this.x = x;
+        this.y = y;
+        this.travelLog = [{this.x, this.y}];
+    }
+}
