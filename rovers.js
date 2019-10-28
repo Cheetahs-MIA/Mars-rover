130e045db3cbcbadf4b2dba0ef17a84542e4f8b1
Vitalii Agapov <vitaly.agapov@outlook.com>
2019-10-28 15:04:19 -0400

    moveForward done

--- rovers.js
+++ rovers.js
@@ -42,6 +42,100 @@ class Rover {
           }
           console.log(`${this.name} is now facing ${this.direction}.`);
         }
+
+    moveForward = (site) => {
+            switch (this.direction) {
+              case 'N':
+                if (this.y > 0) {
+                  switch (site[this.y-1][this.x]) {
+                    case 'free':
+                      site[this.y-1][this.x] = this.name;
+                      site[this.y][this.x] = 'free';
+                      this.y--;
+                      this.travelLog.push({x : this.x, y : this.y});
+                      console.log(`${this.name} moved forward.`);
+                      break;
+                    case 'crater':
+                      console.log('Crater ahead, we can\'t advance!');
+                      break;
+                    default:
+                      console.log('Enemy ahead, we can\'t advance!');
+                      break;
+                  } 
+                } else {
+                  console.log('Can\'t leave test site!');
+                  break;
+                }
+                break;
+              case 'S':
+                if (this.y < 9) {
+                  switch (site[this.y+1][this.x]) {
+                    case 'free':
+                      site[this.y+1][this.x] = this.name;
+                      site[this.y][this.x] = 'free';
+                      this.y++;
+                      this.travelLog.push({x : this.x, y : this.y});
+                      console.log(`${this.name} moved forward.`);
+                      break;
+                    case 'crater':
+                      console.log('Crater ahead, we can\'t advance!');
+                      break;
+                    default:
+                      console.log('Enemy ahead, we can\'t advance!');
+                      break;
+                  } 
+                } else {
+                  console.log('Can\'t leave test site!');
+                  break;
+                }
+                break;
+              case 'E':
+                if (this.x < 9) {
+                  switch (site[this.y][this.x+1]) {
+                    case 'free':
+                      site[this.y][this.x+1] = this.name;
+                      site[this.y][this.x] = 'free';
+                      this.x++;
+                      this.travelLog.push({x : this.x, y : this.y});
+                      console.log(`${this.name} moved forward.`);
+                      break;
+                    case 'crater':
+                      console.log('Crater ahead, we can\'t advance!');
+                      break;
+                    default:
+                      console.log('Enemy ahead, we can\'t advance!');
+                      break;
+                  } 
+                } else {
+                  console.log('Can\'t leave test site!');
+                  break;
+                }
+                break;
+              case 'W':
+                if (this.x > 0) {
+                  switch (site[this.y][this.x-1]) {
+                    case 'free':
+                      site[this.y][this.x-1] = this.name;
+                      site[this.y][this.x] = 'free';
+                      this.x--;
+                      this.travelLog.push({x : this.x, y : this.y});
+                      console.log(`${this.name} moved forward.`);
+                      break;
+                    case 'crater':
+                      console.log('Crater ahead, we can\'t advance!');
+                      break;
+                    default:
+                      console.log('Enemy ahead, we can\'t advance!');
+                      break;
+                  } 
+                } else {
+                  console.log('Can\'t leave test site!');
+                  break;
+                }
+                break;
+            }
+          }
+
     }
 
 const testSite = [
