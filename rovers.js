3836299577b7c27af720b53fbcd05c5d109b21eb
--replace-all <ignaciobellomo@gmail.com>
2019-10-28 14:55:50 -0400

    Ignacio added turning functionality

--- rovers.js
+++ rovers.js
@@ -4,7 +4,55 @@ class Rover {
         this.direction = direction;
         this.x = x;
         this.y = y;
-        this.travelLog = [{this.x, this.y}];
+        this.travelLog = [{x: x, y: y}];
     }
-}
+    
+    turnLeft () {
+        switch (this.direction) {
+            case 'N':
+              this.direction = 'W';
+              break;
+            case 'S':
+              this.direction = 'E';
+              break;
+            case 'E':
+              this.direction = 'N';
+              break;
+            case 'W':
+              this.direction = 'S';
+              break;
+          }
+          console.log(`${this.name} is now facing ${this.direction}.`);
+    }
 
+    turnRight () {
+        switch (this.direction) {
+            case 'N':
+              this.direction = 'E';
+              break;
+            case 'S':
+              this.direction = 'W';
+              break;
+            case 'E':
+              this.direction = 'S';
+              break;
+            case 'W':
+              this.direction = 'N';
+              break;
+          }
+          console.log(`${this.name} is now facing ${this.direction}.`);
+        }
+    }
+
+const testSite = [
+    ['USA', 'free', 'free', 'free', 'free', 'crater', 'free', 'free', 'free', 'China'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'free', 'crater', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'free', 'free', 'crater', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'crater', 'free', 'free', 'free', 'free', 'free', 'crater', 'free', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'crater', 'free', 'free', 'free', 'Russia'],
+  ];
